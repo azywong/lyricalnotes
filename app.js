8be7c61ec88520db23b9354a9ad6d11a759d9d02
azywong <azywong@gmail.com>
2018-04-15 23:41:04 -0700

    up to 2006-11

--- app.js
+++ app.js
@@ -200,7 +200,7 @@ app.get('/alllyrics', function(req, res) {
 
 	var startDate = new Date();
 		startDate.setFullYear(2006);
-		startDate.setMonth(4);
+		startDate.setMonth(10);
 		startDate.setDate(1);
 	var currentDate = startDate;
 	var i = 0;
